f141239a424a8b2c53251041abc498cf14421f2b
Vicent Ciscar <vicentciscar17@gmail.com>
2022-09-15 19:06:57 +0200

    Add Heroes-App files to Monorepo

---
+++ HeroesApp/src/app/heroes/components/heroe-targeta/heroe-targeta.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+
+@Component({
+  selector: 'app-heroe-targeta',
+  templateUrl: './heroe-targeta.component.html',
+  styleUrls: ['./heroe-targeta.component.css']
+})
+export class HeroeTargetaComponent implements OnInit {
+
+  @Input() heroe!: Heroe;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (!this.heroe) {
+      throw new Error('No se ha definido el heroe');
+    }
+  }
+
+}
---
+++ HeroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html',
+  styleUrls: ['./buscar.component.css']
+})
+export class BuscarComponent implements OnInit {
+
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+
+  constructor(private heroesService:HeroesService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  buscando(){
+    this.heroesService.getSugerencias(this.termino).subscribe((heroes) => {
+      this.heroes = heroes;
+    })
+  }
+
+  opcionSelecionada(event: MatAutocompleteSelectedEvent){
+
+    if(!event.option.value){
+      console.log('no hay ningun valor');
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroe(heroe.id!).subscribe((heroe) => {
+      this.heroeSeleccionado = heroe;
+    });
+
+  }
+
+}
